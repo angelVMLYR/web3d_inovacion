8de57df6c4c1535b7dd244b1d7777586b4afa26e
angelVMLYR <angel.rojas@vmlyrcommerce.com>
2023-07-28 12:46:19 -0600

    add fin to vite file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://angelVMLYR.github.io/web3d_inovacion'
+  base: 'https://angelVMLYR.github.io/web3d_inovacion',
+  root: './',
+  build: {
+      outDir: 'dist',
+  },
+  publicDir: 'public'
 })
